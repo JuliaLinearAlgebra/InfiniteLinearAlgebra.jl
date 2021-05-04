7aa3b52b0fafc58be500c5b2cf9c44dc7354871c
Sheehan Olver <solver@mac.com>
2021-05-04 11:40:57 +0100

    Update test_infcholesky.jl

--- test/test_infcholesky.jl
+++ test/test_infcholesky.jl
@@ -8,4 +8,17 @@ using InfiniteLinearAlgebra, LinearAlgebra, BandedMatrices, ArrayLayouts, Test
     # go past adaptive 
     b = [randn(10_000); zeros(∞)]
     @test cholesky(S) \ b ≈ qr(S) \ b ≈ S \ b
+
+    @testset "singularly perturbed" begin
+        ε = 0.0001
+        S = Symmetric(BandedMatrix(0 => 2 .+ ε*(1:∞), 1=> -Ones(∞)))
+        b = [1; zeros(∞)]
+        @test cholesky(S) \ b ≈  S \ b
+    end
+
+    @testset "long bandwidths" begin
+        S = Symmetric(BandedMatrix(0 => 1:∞, 50=> Ones(∞)))
+        b = [1; zeros(∞)]
+        @test cholesky(S) \ b ≈ qr(S) \ b ≈ S \ b
+    end
 end
