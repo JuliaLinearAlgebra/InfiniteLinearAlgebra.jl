387a4976b655f56f87234fbdb18ba24c0e256d3b
Timon S. Gutleb <timon.gutleb@gmail.com>
2024-05-14 07:57:39 -0700

    use AdjorTrans instead

--- src/infcholesky.jl
+++ src/infcholesky.jl
@@ -18,8 +18,8 @@ end
 AdaptiveCholeskyFactors(A::AbstractMatrix{T}) where T = AdaptiveCholeskyFactors(MemoryLayout(A), A)
 MemoryLayout(::Type{AdaptiveCholeskyFactors{T,DM,M}}) where {T,DM,M} = AdaptiveLayout{typeof(MemoryLayout(DM))}()
 
-copy(A::AdaptiveCholeskyFactors) = A
-copy(A::Adjoint{T,<:AdaptiveCholeskyFactors}) where T = copy(parent(A))'
+copy(A::AdaptiveCholeskyFactors) = AdaptiveCholeskyFactors(copy(A.data), copy(A.ncols))
+copy(A::AdjOrTrans{T,<:AdaptiveCholeskyFactors}) where T = copy(parent(A))'
 
 function partialcholesky!(F::AdaptiveCholeskyFactors{T,<:BandedMatrix}, n::Int) where T
     if n > F.ncols
